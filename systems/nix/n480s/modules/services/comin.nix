a8b8a858d104ba165deeffca514236efe4a8c7ea
amurana <alan@murana.uy>
2025-12-14 17:56:02 -0300

    changed comin

--- systems/nix/n480s/modules/services/comin.nix
+++ systems/nix/n480s/modules/services/comin.nix
@@ -3,17 +3,10 @@
 {
   services.comin = {
     enable = true;
-    repository = "github:itDaru/core";
-    path = "/home/user/Dev/core/systems/nix/n480s";
-    revision = "main";
-    
-    # SSH Deploy Key
-    # You will need to generate a new SSH key for this to work
-    # ssh-keygen -t ed25519 -f ~/.ssh/comin_key
-    # And add the public key to your repository's deploy keys.
-    # The private key should be stored in a secure location and 
-    # the path should be updated below.
-    
-    sshKey = "/home/user/.ssh/id_ed25519.pub";
+    remotes = [{
+      name = "origin"
+      url = "https://github.com/itDaru/core/main?dir=systems/nix/n480s";
+      branches.main.name = "main";
+    }];
   };
 }
